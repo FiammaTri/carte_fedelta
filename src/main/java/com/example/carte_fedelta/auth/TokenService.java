5bbb20b55157fe8c1f0aa9cdd3ed27d1e5a86b7a
FiammaTri <scinty-pcd@live.it>
2025-04-10 15:52:07 +0200

    Creazione package e classi

---
+++ src/main/java/com/example/carte_fedelta/auth/TokenService.java
@@ -0,0 +1,8 @@
+/*
+package com.example.carte_fedelta.auth;
+
+public class TokenService {
+
+}
+*/
+//NON UTILE PER IL MOMENTO
---
+++ src/main/java/com/example/carte_fedelta/model/Card.java
@@ -0,0 +1,5 @@
+package com.example.carte_fedelta.model;
+
+public class Card {
+
+}
