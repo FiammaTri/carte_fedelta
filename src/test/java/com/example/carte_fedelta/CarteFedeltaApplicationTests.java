1ea6cc1040df43f8787aa0b2025fa73e7a85ef73
FiammaTri <scinty-pcd@live.it>
2025-04-10 14:23:58 +0200

    Inizio progetto

---
+++ src/test/java/com/example/carte_fedelta/CarteFedeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carte_fedelta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteFedeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
