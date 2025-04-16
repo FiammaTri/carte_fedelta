694befa96e25f1b6bf8ec3448cc7c5c4689b9577
FiammaTri <scinty-pcd@live.it>
2025-04-16 16:12:28 +0200

    creazione REST POST per aggiunta carta

--- src/main/java/com/example/carte_fedelta/controller/CardController.java
+++ src/main/java/com/example/carte_fedelta/controller/CardController.java
@@ -6,8 +6,14 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.example.carte_fedelta.model.Card;
+import com.example.carte_fedelta.model.Store;
 import com.example.carte_fedelta.repository.CardRepository;
+import com.example.carte_fedelta.repository.StoreRepository;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -18,6 +24,9 @@ public class CardController {
 	
 	@Autowired
 	private CardRepository cardRepository;
+	
+	@Autowired
+	private StoreRepository storeRepository;
 	
 	// metodo che restituisce tutte le card
 	@GetMapping
@@ -38,4 +47,17 @@ public class CardController {
 		
 		return card.get();
 	}
-}
+	
+	// metodo per creare una card
+	@PostMapping("/{idStore}")
+	public Object createCard(@PathVariable("idStore") Long id, @RequestBody Card card, HttpServletResponse response) {
+		Optional<Store> store = storeRepository.findById(id);
+		if (!store.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("message", "Store non trovato"));
+		}
+	Card newCard = new Card();
+	newCard.setId(null);
+	newCard.setNumber(card.getNumber());
+	newCard.setStore_name(store.get());
+	return cardRepository.save(newCard);}
+	}
