eb33f12fcf7fbd64b73981d62d5b322fce30db0f
FiammaTri <scinty-pcd@live.it>
2025-05-16 16:48:34 +0200

    merge

--- src/main/java/com/example/carte_fedelta/repository/StoreRepository.java
+++ src/main/java/com/example/carte_fedelta/repository/StoreRepository.java
@@ -1,6 +1,5 @@
 package com.example.carte_fedelta.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +7,6 @@ import com.example.carte_fedelta.model.Store;
 
 @Repository
 public interface StoreRepository extends JpaRepository <Store, Long>{
+	
 	
 }
